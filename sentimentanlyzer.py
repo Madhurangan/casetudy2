9b984126f9e44e02600ec97dfa150ccf17f41608
Madhurangan <82032180+Madhurangan@users.noreply.github.com>
2023-07-16 20:53:26 +0530

    Update sentimentanlyzer.py

--- sentimentanlyzer.py
+++ sentimentanlyzer.py
@@ -1,21 +1,23 @@
 import streamlit as st
-from transformers import pipeline
+from model import train_model, classify_text
 
-st.title('Text classification App')
-st.write('This app uses the Hugging Face Transformers [sentiment analyser](https://huggingface.co/course/chapter1/3?fw=tf) library to clasify the sentiment of your input as postive or negative. The web app is built using [Streamlit](https://docs.streamlit.io/en/stable/getting_started.html).')
+# Load the model
+vectorizer, model, embeddings, cluster_labels, cluster_types = train_model('dataaaa.csv')
 
+# Streamlit app
+def main():
+    st.title('Text Category Detection')
 
-form = st.form(key='sentiment-form')
-user_input = form.text_area('Enter your text')
-submit = form.form_submit_button('Submit')
+    # User input
+    text_input = st.text_input('Enter a text sample:', '')
 
-if submit:
-    classifier = pipeline("sentiment-analysis")
-    result = classifier(user_input)[0]
-    label = result['label']
-    score = result['score']
+    # Classify user input
+    if text_input:
+        # Classify the text
+        category_type = classify_text(text_input, vectorizer, model, embeddings, cluster_labels, cluster_types)
 
-    if label == 'POSITIVE':
-        st.success(f'{label} sentiment (score: {score})')
-    else:
-        st.error(f'{label} sentiment (score: {score})')
+        # Display the detected category type
+        st.write(f'Detected Category Type: {category_type}')
+
+if __name__ == '__main__':
+    main()
